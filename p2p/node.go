1a284729e45f36d6b60e46b729cd3c04c236ca98
Mik Iwanowski <info@slnt-opp.xyz>
2023-03-09 21:39:22 +0100

    add bootstrap node

--- p2p/node.go
+++ p2p/node.go
@@ -65,6 +65,7 @@ func CreateNode(ctx context.Context, inputKey string, port int, handler network.
 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
 	}
 
 	// Convert Bootstap Nodes into usable addresses.
